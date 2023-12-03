ad29f406bc08c966ab365f6a0b54cf3734b51587
BionicScout <90854007+BionicScout@users.noreply.github.com>
2023-12-03 14:21:54 -0500

    Finished Commenting

--- Networking/server.js
+++ Networking/server.js
@@ -13,33 +13,38 @@ const server = http.createServer((req, res) => {
     console.log("Request Made");
     console.log(req.url, req.method);
     console.log(req.socket);
-    
 
     //Select Correct Request
-    if(req.url == "/Networking/writeContact"){
-        writeContact(req, res);
-    }
-    else if(req.url == '/Networking/checkPassword'){
-        checkPassword(req, res);
-    }
-    else if(req.url == '/Networking/checkUsername'){
-        checkUsername(req, res);
-    }
-    else if(req.url == '/Networking/createProfile'){
-        createProfile(req, res);
-    }
-    else{
+    if(backendDelegator(res, req) == false){
         htmlPageRequest(req, res);
-    }
-
+    }    
 
     console.log("\n");
 });
-
-
 
 //(port number, host name, ...)
 let port = 4870;
 server.listen(port, 'localhost', () => {
     console.log("Listening for request on port " + port);
 });
+
+function backendDelegator(res, req){
+    if(req.url == "/Networking/writeContact"){
+        writeContact(req, res);
+        return true;
+    }
+    else if(req.url == '/Networking/checkPassword'){
+        checkPassword(req, res);
+        return true;
+    }
+    else if(req.url == '/Networking/checkUsername'){
+        checkUsername(req, res);
+        return true;
+    }
+    else if(req.url == '/Networking/createProfile'){
+        createProfile(req, res);
+        return true
+    }
+   
+    return false;
+}
